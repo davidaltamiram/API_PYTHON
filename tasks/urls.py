70eec8bd747ea02748eb86c488d367141f97592f
David Altamira <davidaltamira19@gmail.com>
2023-10-08 19:34:31 -0600

    API REST

---
+++ tasks/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
+from rest_framework import routers
+from . import views
+
+#Abreviado de versiones de api
+router = routers.DefaultRouter()
+router.register(r'tasks', views.TaskView, 'tasks')
+
+urlpatterns = [
+    path("api/v1/", include(router.urls)),
+    path('docs/', include_docs_urls(title="TASK API"))
+]
+
+
